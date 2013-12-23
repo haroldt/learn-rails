87493cd471a4297598055c6f0e878973f44323da
Harold H Teunisse <geek4law@gmail.com>
2013-12-23 19:50:56 +0000

    Initial commit.  Bundle not installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LearnRails::Application.config.secret_key_base = 'd696c976560c61fe376ab93f97bf268fd5a89b6943db3ba53459dca4c00e1b8d25f931c972ea336b6964f8393aa45e69cd96b5462e2d59a441f5e064d9ec84a9'
